973446aff333074466bca917b6ca7392bd37e491
Gabri3el <g_gabrielmerino@yahoo.com.br>
2021-07-13 14:36:29 -0300

    heroku config

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,10 @@ module.exports = {
       pool: {
         min: 2,
         max: 10,
-        propagateCreateError: false
+        propagateCreateError: false, 
+        idleTimeoutMillis: 10000, 
+        createTimeoutMillis: 10000, 
+        acquireTimeoutMillis: 10000
       },
       migrations: {
         tableName: 'knex_migrations'
